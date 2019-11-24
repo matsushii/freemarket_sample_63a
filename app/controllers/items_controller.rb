19def4bd12a0cfd7caa2c3faf185dadfdbebf188
kyo02225 <quon.kyo.suica02225@gmail.com>
2019-11-24 17:45:29 +0900

    item indexアクション追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,8 @@
 class ItemsController < ApplicationController
   def index
+    @mens_items = Item.where(id: 1..5)
+    @ladies_items = Item.where(id: 6..11)
+    @elec_items = Item.where(id: 12..15)
+    @hobby_おもちゃ・ホビー・グッズitems = Item.where(id: 16..20)
   end
 end
