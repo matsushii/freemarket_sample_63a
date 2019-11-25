a8f1ef5739c4dcc29f7464741e72f903cd2541ab
shanghuifu <shanghuifu@gmail.com>
2019-11-25 17:36:55 +0900

    update application_controller.rb
    コメントアウトを削除

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,7 +19,6 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    # binding.pry
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :first_name, :last_name, :birthday])
   end
 end
