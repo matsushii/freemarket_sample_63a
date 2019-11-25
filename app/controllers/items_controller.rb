b35403224ec4018a8a1cc76c498241eeba5a73be
kyo02225 <quon.kyo.suica02225@gmail.com>
2019-11-25 21:15:28 +0900

    Update items_controller.rb

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,8 @@
 class ItemsController < ApplicationController
   def index
-    @mens_items = Item.where(id: 1..5)
-    @ladies_items = Item.where(id: 6..11)
-    @e_appliances_items = Item.where(id: 12..15)
-    @hobbies_items = Item.where(id: 16..20)
+    @mens_items = Item.where(id: 1..5).order(created_at: :desc).limit(10)
+    @ladies_items = Item.where(id: 6..11).order(created_at: :desc).limit(10)
+    @e_appliances_items = Item.where(id: 12..15).order(created_at: :desc).limit(10)
+    @hobbies_items = Item.where(id: 16..20).order(created_at: :desc).limit(10)
   end
 end
