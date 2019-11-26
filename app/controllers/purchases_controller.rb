bfc2c6456b358b12909ce57c40addf120c346339
kyo02225 <quon.kyo.suica02225@gmail.com>
2019-11-26 20:53:03 +0900

    create actionが遷移する様に追加。

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,20 +1,10 @@
 class PurchasesController < ApplicationController
   def new
     @item = Item.find_by(id: params[:item_id])
-    # @purchase = Purchase.new
   end
   
   def create
-    binding.pry
-    # @item = Item.find_by(id: params[:item_id])
-    # @item = Item.find_by(id: params[:item_id])
-    purchase = params.merge(user_id: current_user.id)
-    Purchase.create
-    redirect_to item_path(params[:item_id])
-  end
-
-  private 
-  def purchase_params
-    params.merge(user_id: current_user.id, item_id: params[:item_id])
+    Purchase.create(user_id: current_user.id,
+                            item_id: params[:item_id])
   end
 end
