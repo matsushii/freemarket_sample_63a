772c55530b1f538be4a84d8396a922da05a50c2e
kyo02225 <quon.kyo.suica02225@gmail.com>
2019-11-26 17:32:57 +0900

    ルーティングをネスト

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "items#index"
-  resources :items, only: [:index, :show]
+  resources :items, only: [:index, :show] do
+    resources :purchases, only: [:new, :create]
+  end
   resources :users, only: :show
 end
