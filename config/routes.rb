fb0de01f1fb58b763971703ecf5e2e3140397072
kyo02225 <quon.kyo.suica02225@gmail.com>
2019-11-26 13:03:03 +0900

    routes修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
   root to: "items#index"
-  # resources :items, only: [:index, :show]
-  get "items" => "items#show"
+  resources :items, only: [:index, :show]
 end
