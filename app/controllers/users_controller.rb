c7eee154301feb643a2f5a95c046a0b94d78d70d
shanghuifu <shanghuifu@gmail.com>
2019-11-27 13:56:48 +0900

    editとupdateの実装
    editアクション、updateアクションそれぞれにメソッド記述。
    updateできなかった場合の処理も併せて記述

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,11 +5,23 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find(params[:id])
   end
 
   def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      redirect_to user_path(current_user)
+    else
+      render 'edit'
+    end
   end
-  
+
   def logout
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:nickname, :introduction)
   end
 end
