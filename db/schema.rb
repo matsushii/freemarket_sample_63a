6c6f2a86933a599f769703bd4d66767664b0a282
shanghuifu <shanghuifu@gmail.com>
2019-12-02 21:52:30 +0900

    add phone number & update step2
    ユーザー情報入力後、入力した情報を維持したまま電話番号登録（SMS認証）ページに遷移する。

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_27_220528) do
+ActiveRecord::Schema.define(version: 2019_12_02_115554) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "postal_code", null: false
@@ -74,6 +74,7 @@ ActiveRecord::Schema.define(version: 2019_11_27_220528) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "phone_number"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
