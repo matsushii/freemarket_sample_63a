63bfd74592b75bc9af2afde9a29ee58d0c9950b8
kyo02225 <quon.kyo.suica02225@gmail.com>
2019-12-03 13:14:41 +0900

    細かい修正

--- app/assets/javascripts/show.js
+++ app/assets/javascripts/show.js
@@ -1,7 +1,6 @@
 $(document).on('turbolinks:load',function(){
   $('.items-photo__lower__top__box--slide').on('mouseover', function(){
     $('detail__body--left--image').attr("src", $(this).attr("src"))
-    $(this).css('cursor', 'pointer')
   });
 
   $('.items-photo__lower__top__box--slide').on('mouseout', function(){
