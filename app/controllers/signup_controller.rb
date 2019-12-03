8f1788816ad4811e9969f3059b185d168d088f78
shanghuifu <shanghuifu@gmail.com>
2019-12-03 15:29:19 +0900

    update signup controller
    各ステップの入力情報をsessionに入れて保持する。また、それぞれのステップでバリデーションに引っかかった場合にrenderして引き戻す

--- app/controllers/signup_controller.rb
+++ app/controllers/signup_controller.rb
@@ -1,16 +1,29 @@
 class SignupController < ApplicationController
+  before_action :save_step1_to_session, only: :step2
+  before_action :save_step2_to_session, only: :step3
+
   def step1
     @user = User.new
-    @user.addresses.build
   end
 
   def step2
-    session[:user_params] = user_params
     @user = User.new
-    @user.addresses.build
+  end
+  
+  def step3
+    @user = User.new
+    @user.build_address #userモデルとaddressモデルを関連付け
   end
 
-  def step3
+  def create
+    @user = User.new(session[:user_params_afeter_step2]) #引数としてUserのsessionを渡す
+    @user.build_address(user_params[:address_attributes]) #step3で入力された情報を代入
+    if @user.save
+      session[:id] = @user.id #idをsessionに入れてログイン状態にできる
+      redirect_to complete_signup_index_path
+    else
+      render '/signup/step1'
+    end
   end
 
   private
@@ -24,8 +37,30 @@ class SignupController < ApplicationController
       :namae_kanji,
       :myoji_kana,
       :namae_kana,
+      :birthday,
       :phone_number,
-      addresses_attributes: [:addresses]
+      address_attributes: [
+        :id,
+        :postal_code,
+        :prefecture,
+        :city,
+        :address,
+        :building
+      ]
     )
   end
+
+  def save_step1_to_session
+    session[:user_params_afeter_step1] = user_params #step1で入力された情報を入れる
+    @user = User.new(session[:user_params_afeter_step1])
+    render '/signup/step1' unless @user.valid?
+  end
+  
+  def save_step2_to_session
+    session[:user_params_afeter_step2] = user_params #step2で入力された情報をいれる
+    session[:user_params_afeter_step2].merge!(session[:user_params_afeter_step1])
+    @user = User.new
+    render '/signup/step2' unless user_params[:phone_number].present?
+  end
+  
 end
