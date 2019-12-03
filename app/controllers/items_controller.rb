ef8b3a465434967da01c93f2073085ffcb4323fc
kyo02225 <quon.kyo.suica02225@gmail.com>
2019-12-03 11:41:51 +0900

    不要な記述を削除

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,7 +7,6 @@ class ItemsController < ApplicationController
     @item = Item.new
     @item.images.build
     @image = Image.new
-   
   end
   
   def create
@@ -23,7 +22,6 @@ class ItemsController < ApplicationController
   def show
     @item = Item.find(params[:id])
     @item_images = @item.images.limit(10)
-    # render  layout: false
   end
 
   private
@@ -32,5 +30,3 @@ class ItemsController < ApplicationController
   end
 end
 
-# /Users/saeki/projects/freemarket_sample_63a/public/uploads/image/image/8/sample_item.jpeg
-"/uploads/image/image/1/sample_item.jpg"
