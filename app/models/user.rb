df1c4bac7ad0a1e60f518d1aacbd01e29005ddff
shanghuifu <shanghuifu@gmail.com>
2019-12-03 14:08:40 +0900

    update step3(住所登録)
    アソシエーションの変更。ビューの実装。登録完了画面を含む。

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :addresses
+  has_one :address
   has_many :items
   has_many :purchases
   has_many :cards
-  accepts_nested_attributes_for :addresses, allow_destroy: true
+  
+  accepts_nested_attributes_for :address, allow_destroy: true
   validates :nickname, :myoji_kanji, :namae_kanji, :myoji_kana, :namae_kana, :birthday, presence: true
 end
