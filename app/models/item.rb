ea901f8a32f55fa0fe50bdffc5e560c40f54a5f7
kyo02225 <quon.kyo.suica02225@gmail.com>
2019-12-04 11:15:20 +0900

    condition,shipping_date,shipping_feeのenumを追加。

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,8 +6,21 @@ class Item < ApplicationRecord
   accepts_nested_attributes_for :images, allow_destroy: true
   validates :name, presence: true
 
+  enum condition: {
+    新品、未使用:1,
+    未使用に近い:2,
+    目立った傷や汚れなし:3,
+    やや傷や汚れあり:4,
+    傷や汚れあり:5,
+    全体的に状態が悪い:6,
+  }
+
+  enum shipping_fee: {
+    送料込み（出品者負担）:1,
+    着払い（購入者負担）:2,
+  }
+
   enum shipping_from: {
-    "---":0,
     北海道:1,
     青森県:2,
     岩手県:3,
@@ -55,5 +68,10 @@ class Item < ApplicationRecord
     宮崎県:45,
     鹿児島県:46,
     沖縄県:47
+  }
+  enum shipping_date: {
+    １〜２日で発送:1,
+    ２〜３日で発送:2,
+    ４〜７日で発送:3,
   }
 end
