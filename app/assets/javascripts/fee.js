352cc83ea52f9fdf67a004e83c5afa61e08e1a8f
kyo02225 <quon.kyo.suica02225@gmail.com>
2019-12-04 11:53:50 +0900

    商品出品手数料反映

---
+++ app/assets/javascripts/fee.js
@@ -0,0 +1,15 @@
+$(function(){
+    $('.price-filed').on('input', function(){
+      var inputPrice = $(this).val();
+      if(inputPrice >= 300 && inputPrice <= 9999999){
+        var commission = Math.floor(inputPrice * 0.1).toLocaleString();
+        var profit = Math.ceil(inputPrice * 0.9).toLocaleString();
+        $('.fee__right').text("¥" + commission);
+        $('.profit').text("¥" + profit);
+      }else{
+        $('.fee__right').text("-");
+        $('.profit').text("-");
+      }
+    });
+  });
+  
