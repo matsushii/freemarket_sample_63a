9ca1e053d030fabdbb92f5f7bfe9835a3667dd31
shanghuifu <shanghuifu@gmail.com>
2019-12-05 15:04:52 +0900

    コントローラーにmypageアクションを定義
    before_actionのset_userにmypageアクションを含めました

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,10 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :update]
+  before_action :set_user, only: [:show, :edit, :update, :mypage]
 
+  def mypage
+    @nickname = @user.nickname
+  end
+  
   def show
     @nickname = @user.nickname
   end
