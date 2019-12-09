24994a26c33fb6c078fdf8afdf08c0da02af527a
matsushii <mtsmath24@yahoo.co.jp>
2019-12-09 10:49:26 +0900

    カード購入時の処理を初回と2回目で条件分岐
    カード購入時の初回と2回目以降で条件分岐するために、purchases.newに条件分岐を記述しました。

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -32,7 +32,20 @@ class PurchasesController < ApplicationController
         customer = Payjp::Customer.create(card: params['payjp-token'])
         @card = Card.new(user_id: current_user.id, customer_id: customer.id, card_id: customer.default_card)
         if @card.save
-          redirect_to root_path, notice: 'カードの登録が完了しました'
+          @purchase = Purchase.new(user_id: current_user.id, item_id: purchase_params[:item_id])
+          @card = Card.find_by(user_id: current_user.id)
+          Payjp.api_key = 'sk_test_7e45cce1c3bf4742a63222c4'
+          Payjp::Charge.create(
+          amount: @purchase.item.price,
+          customer: @card.customer_id, 
+          currency: 'jpy', 
+          )
+          if @purchase.save
+            redirect_to root_path, notice: '購入が完了しました'
+          else
+            flash.now[:alert] = 'エラーが発生しました'
+            render :new
+          end    
         else
           redirect_to action: "create"
         end
