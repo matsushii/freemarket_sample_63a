637dbffc990d2b5d0011b0ebff54c5350e6bcebc
shanghuifu <shanghuifu@gmail.com>
2019-12-11 17:38:12 +0900

    signupコントローラーの修正
    共通のメソッドはbefore_actionで呼ぶようにしました。新しく作成したcreate_cardアクションを定義しました。ステップの追加に伴い、リダイレクトのパスを変更しました。

--- app/controllers/signup_controller.rb
+++ app/controllers/signup_controller.rb
@@ -1,4 +1,5 @@
 class SignupController < ApplicationController
+  before_action :build_user, only: [:step1, :step2, :step3, :step4]
   before_action :save_step1_to_session, only: :step2
   before_action :save_step2_to_session, only: :step3
 
@@ -6,16 +7,33 @@ class SignupController < ApplicationController
   end
   
   def step1
-    @user = User.new
   end
 
   def step2
-    @user = User.new
   end
   
   def step3
+    @user.build_address #userモデルとaddressモデルを関連付けたインスタンスを生成
+  end
+
+  def step4
+    @user.cards.build #userモデルとcardモデルを関連付けたインスタンスを生成
+  end
+
+  def create_card
     @user = User.new
-    @user.build_address #userモデルとaddressモデルを関連付け
+    Payjp.api_key = 'sk_test_7e45cce1c3bf4742a63222c4'
+      if params['payjp-token'].blank?
+        redirect_to action: "step4"
+      else
+        customer = Payjp::Customer.create(card: params['payjp-token'])
+        @card = Card.new(user_id: current_user.id, customer_id: customer.id, card_id: customer.default_card)
+        if @card.save
+          redirect_to complete_signup_index_path
+        else
+          redirect_to action: "step4"
+        end
+      end
   end
 
   def create
@@ -23,14 +41,18 @@ class SignupController < ApplicationController
     @user.build_address(user_params[:address_attributes]) #step3で入力された情報を代入
     if @user.save
       session[:id] = @user.id #idをsessionに入れてログイン状態にできる
-      redirect_to complete_signup_index_path
+      redirect_to step4_signup_index_path
       sign_in @user
     else
       render '/signup/step1'
     end
   end
-
+  
   private
+  def build_user
+    @user = User.new
+  end
+
   def user_params
     params.require(:user).permit(
       :nickname,
@@ -64,7 +86,7 @@ class SignupController < ApplicationController
       render '/signup/step1' unless @user.valid?
     end
   end
-  
+
   def save_step2_to_session
     if params[:user].blank?
       @user = User.new
