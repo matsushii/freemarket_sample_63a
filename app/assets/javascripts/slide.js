41b24977650d58f7190e10ee5eb41db89ca35da5
kyo02225 <quon.kyo.suica02225@gmail.com>
2019-12-12 21:29:00 +0900

    slide.jsのturbolinksの記述を変更

--- app/assets/javascripts/slide.js
+++ app/assets/javascripts/slide.js
@@ -1,7 +1,9 @@
-$(document).ready(function(){
-  $('.news').bxSlider({
-      auto: true,
-      pause: 5000,
-      useCSS: false,
+$(document).on('turbolinks:load', function() {
+  $(document).ready(function(){
+    $('.news').bxSlider({
+        auto: true,
+        pause: 5000,
+        useCSS: false,
+    });
   });
 });
