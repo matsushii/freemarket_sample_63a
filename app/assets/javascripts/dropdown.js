a50e83b111788f894f200cc1a387a850fcbd8629
shanghuifu <shanghuifu@gmail.com>
2019-12-24 17:23:12 +0900

    update dropdown.js

--- app/assets/javascripts/dropdown.js
+++ app/assets/javascripts/dropdown.js
@@ -1,19 +1,21 @@
-$(function(){
-  $('.category-search__link').hover(function(){
-    $('.category-nav-parents').show();
-  }, function(){
-    $('.category-nav-parents').hide();
-  });
-
-  $('.category-nav-parents__parent').hover(function(){
-    $(this).find('.category-nav-children').show();
-  }, function(){
-    $(this).find('.category-nav-children').hide();
-  });
-
-  $('.category-nav-children__child').hover(function(){
-    $(this).find('.category-nav-grandchildren').show();
-  }, function(){
-    $(this).find('.category-nav-grandchildren').hide();
+$(document).on('turbolinks:load', function(){
+  $(function(){
+    $('.category-search__link').hover(function(){
+      $('.category-nav-parents').show();
+    }, function(){
+      $('.category-nav-parents').hide();
+    });
+  
+    $('.category-nav-parents__parent').hover(function(){
+      $(this).find('.category-nav-children').show();
+    }, function(){
+      $(this).find('.category-nav-children').hide();
+    });
+  
+    $('.category-nav-children__child').hover(function(){
+      $(this).find('.category-nav-grandchildren').show();
+    }, function(){
+      $(this).find('.category-nav-grandchildren').hide();
+    });
   });
 });
